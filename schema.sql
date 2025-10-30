3fdf64c3537099f348110a71b4d99b28f2dc7f24
omerfarukekmekci <omerfarukekmekci2004@gmail.com>
2025-10-30 21:09:36 +0300

    schema.sql updated to include Ömer Faruk Ekmekçi's tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE Countries (
     country_id INTEGER PRIMARY KEY,
     population BIGINT,
@@ -8,6 +7,40 @@ CREATE TABLE Countries (
     longitude FLOAT,
 );
 CREATE INDEX idx_countries_region ON Countries(region);
+
+-- Production table
+CREATE TABLE Production (
+    production_ID SERIAL PRIMARY KEY,
+    country_code INTEGER NOT NULL,
+    commodity_code INTEGER NOT NULL,
+    item_name VARCHAR(128) NOT NULL,
+    year INTEGER NOT NULL,
+    unit varchar(7),
+    quantity DECIMAL(12, 3),
+    
+    FOREIGN KEY (country_code) REFERENCES Countries(country_code) 
+        ON DELETE CASCADE,
+    FOREIGN KEY (commodity_code) REFERENCES Commodities(commodity_code) 
+        ON DELETE CASCADE,
+    
+    UNIQUE(country_code, commodity_code, year)
+);
+CREATE INDEX idx_production_country_year ON Production(country_code, year);
+CREATE INDEX idx_production_commodity_year ON Production(commodity_code, year);
+
+-- Producer_Prices table
+CREATE TABLE Producer_Prices (
+    production_value_ID SERIAL PRIMARY KEY,
+    production_ID INTEGER NOT NULL,
+    element VARCHAR(56),
+    year INTEGER NOT NULL,
+    unit VARCHAR(9),
+    value DECIMAL(15, 3),
+    
+    FOREIGN KEY (production_ID) REFERENCES Production(production_ID) 
+        ON DELETE CASCADE,
+
+);
 
 
 -- Trade_Flows table
@@ -36,7 +69,6 @@ CREATE TABLE Trade_Flows (
 CREATE INDEX idx_trade_flows_exporter_year ON Trade_Flows(reporter_country, year);
 CREATE INDEX idx_trade_flows_importer_year ON Trade_Flows(partner_country, year);
 CREATE INDEX idx_trade_flows_commodity_year ON Trade_Flows(trade_item, year);
-=======
 -- Land_Use table
 CREATE TABLE Land_Use (
     unique_id SERIAL PRIMARY KEY,
@@ -55,4 +87,3 @@ CREATE TABLE Investments (
     expenditure_value INTEGER,
     expenditure_type VARCHAR(100)
 );
->>>>>>> 81239a213539ee3780a7bd556b1226c49dfae5d7
