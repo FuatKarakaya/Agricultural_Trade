5e3a60e7f5766ca016b6c252cb4433ece3352cc5
FuatKarakaya <fuatkarakaya00@gmail.com>
2025-11-25 21:27:55 +0300

    minor

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,9 @@
 from .mainRouting import main_bp
 from .countryRouting import country_bp
-#from .commodityRouting import commodity_bp
+from .commodityRouting import commodity_bp
 from .tradeRouting import trade_bp
-#from .pricesRouting import prices_bp
+from .consumerPriceRouting import consumer_price_bp
+from .producerPriceRouting import producer_price_bp
 from .landuseRouting import landuse_bp
 from .prodRouting import prod_bp
 from .prodValRouting import prod_val_bp
@@ -10,9 +11,10 @@ from .prodValRouting import prod_val_bp
 __all__ = [
     "main_bp",
     "country_bp",
-   # "commodity_bp",
+    "consumer_price_bp",
+    "producer_price_bp",
+    "commodity_bp",
     "trade_bp",
-   # "prices_bp",
     "landuse_bp",
     "prod_bp",
     "prod_val_bp",
