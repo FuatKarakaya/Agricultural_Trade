f5f3caad2a46973ae0530547309d378ef4125ff3
omerfarukekmekci <omerfarukekmekci2004@gmail.com>
2025-11-26 09:22:26 +0300

    worked on some tables

--- temp.py
+++ temp.py
@@ -2,4 +2,10 @@ from database import execute_query
 
 if __name__ == "__main__":
 
-    execute_query('''UPDATE trade_data_long SET partner_countries = countries.country_id FROM countries WHERE trade_data_long.partner_countries = countries.country_name;''')
+    X = execute_query(
+        """
+        COPY staging_prod FROM '/prod.csv' DELIMITER ',' CSV HEADER;
+        """
+    )
+
+    print(X)
