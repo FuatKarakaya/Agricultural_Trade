4a6cc09fa03e56181ebf4269f2d4156916fadee7
kalayfurkan <maclicente@gmail.com>
2025-11-26 12:02:31 +0300

    __init__.py fixed

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,19 +1,21 @@
 from .mainRouting import main_bp
 from .countryRouting import country_bp
-#from .commodityRouting import commodity_bp
+from .commodityRouting import commodity_bp
 from .tradeRouting import trade_bp
-#from .pricesRouting import prices_bp
 from .landuseRouting import landuse_bp
 from .prodRouting import prod_bp
 from .prodValRouting import prod_val_bp
+from .consumerPriceRouting import consumer_price_bp
+from .producerPriceRouting import producer_price_bp
 
 __all__ = [
     "main_bp",
     "country_bp",
-   # "commodity_bp",
+    "commodity_bp",
     "trade_bp",
-   # "prices_bp",
     "landuse_bp",
     "prod_bp",
     "prod_val_bp",
+    "consumer_price_bp",
+    "producer_price_bp",
 ]
